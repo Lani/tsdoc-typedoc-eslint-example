36b0fa63a70a2558297839f3d8aa84135daae57b
Niklas Lagergren <niklas.lagergren@consafelogistics.com>
2021-03-09 13:01:42 +0100

    First commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+
+export class Math {
+  /**
+   * Returns the sum of two numbers.
+   *
+   * @remarks
+   * This is our math utilities lib for shared projects.
+   *
+   * @param x - The first input number
+   * @param y - The second input number
+   * @returns The sum of `x` and `y`
+   */
+  public add(x: number, y: number): number {
+    return x + y;
+  }
+
+
+  /**
+   * 
+   * @param x test
+   * @param y west
+   * @returns weast
+   * 
+   */
+  public subtract(x: number, y: number): number {
+    return x - y;
+  }
+}
